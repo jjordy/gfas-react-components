4e68dc5cd4b8547ab94e48748a4fa18e76dacc7f
Jordan Addison <jordanrileyaddison@gmail.com>
2017-03-02 16:44:27 -0700

    update default exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 export {Accordion, AccordionHeading, AccordionItem} from './Accordion'
+export {default as AFK} from './AFK'
+export {default as Await} from './Await'
 export {default as Base} from './Base/'
 export {default as Box} from './Layout/Box/'
 export {default as Button} from './Button/'
